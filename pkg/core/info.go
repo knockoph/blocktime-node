ad6827744e70031deaa29bb41374e35e1c71372d
knockoph <knockoph@proton.me>
2025-01-28 22:06:21 +0100

    using rwmutex instead of mutex to access cached blocktime

--- pkg/core/info.go
+++ pkg/core/info.go
@@ -8,12 +8,12 @@ import (
 type Info struct {
 	blocks int
 	client *Client
-	mu     *sync.Mutex
+	mu     *sync.RWMutex
 }
 
 func NewInfo(rpcURL string, username string, password string) *Info {
 	infoClient := NewClient(rpcURL, username, password)
-	var infoMu sync.Mutex
+	var infoMu sync.RWMutex
 	info := Info{
 		blocks: 0,
 		client: infoClient,
@@ -23,16 +23,18 @@ func NewInfo(rpcURL string, username string, password string) *Info {
 }
 
 func (info *Info) GetBlocks(forceUpdate bool) (int, error) {
-	info.mu.Lock()
-	defer info.mu.Unlock()
-
 	if info.blocks != 0 && !forceUpdate {
+		info.mu.RLock()
+		defer info.mu.RUnlock()
 		return info.blocks, nil
 	}
 
+	info.mu.Lock()
+	defer info.mu.Unlock()
+
 	result, err := CallBlockchainInfo(info.client)
 	if err != nil {
-		return info.blocks, fmt.Errorf("error in get blocks: %w", err)
+		return 0, fmt.Errorf("error in get blocks: %w", err)
 	}
 
 	info.blocks = result.Blocks
